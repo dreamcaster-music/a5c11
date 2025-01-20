9f5aeda4460b6a5d5b1f00f1dac4d1b88e3a0cf3
Ronin <126512548+eudov@users.noreply.github.com>
2025-01-20 12:40:08 -0600

    windows terminal size fn
    Added a ridiculous amount of imports from windows THANKS mr Gates

--- src/core.rs
+++ src/core.rs
@@ -3,6 +3,17 @@ use std::mem;
 #[cfg(unix)]
 use libc::{ioctl, winsize, STDOUT_FILENO, TIOCGWINSZ};
 
+#[cfg(windows)]
+use winapi::um::handleapi::INVALID_HANDLE_VALUE;
+#[cfg(windows)]
+use winapi::um::processenv::GetStdHandle;
+#[cfg(windows)]
+use winapi::um::winbase::STD_OUTPUT_HANDLE;
+#[cfg(windows)]
+use winapi::um::wincon::GetConsoleScreenBufferInfo;
+#[cfg(windows)]
+use winapi::um::wincon::CONSOLE_SCREEN_BUFFER_INFO;
+
 /// Returns the size of the terminal window.
 ///
 /// More specifically, this will return the width and height of the terminal
@@ -25,7 +36,27 @@ pub fn terminal_size() -> Option<(u16, u16)> {
 
     // Put windows code here
     #[cfg(windows)]
-    {}
+    {
+        unsafe {
+            let handle = GetStdHandle(STD_OUTPUT_HANDLE);
+            if handle == INVALID_HANDLE_VALUE {
+                return None;
+            }
+
+            let mut csbi: CONSOLE_SCREEN_BUFFER_INFO = mem::zeroed();
+            if GetConsoleScreenBufferInfo(handle, &mut csbi) != 0 {
+                // Convert i16 to u16 using try_into()
+                return Some((
+                    (csbi.srWindow.Right - csbi.srWindow.Left + 1)
+                        .try_into()
+                        .unwrap(),
+                    (csbi.srWindow.Bottom - csbi.srWindow.Top + 1)
+                        .try_into()
+                        .unwrap(),
+                ));
+            }
+        }
+    }
 
     None
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 mod core;
 
-fn main() {}
+fn main() {
+    match core::terminal_size() {
+        Some((width, height)) => {
+            println!("Terminal size: {} columns and {} rows", width, height);
+        }
+        None => {
+            println!("Error! Unable to retrieve terminal size...");
+        }
+    }
+}
