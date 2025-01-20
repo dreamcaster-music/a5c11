f9af9bf9c14efa59513847f53a84287b606dcb00
Wes <wesley.studt@gmail.com>
2025-01-20 11:27:09 -0600

    added documentation

---
+++ src/core.rs
@@ -0,0 +1,26 @@
+use std::mem;
+
+#[cfg(unix)]
+use libc::{ioctl, winsize, STDOUT_FILENO, TIOCGWINSZ};
+
+/// Returns the size of the terminal window.
+///
+/// More specifically, this will return the width and height of the terminal
+/// window measured in how many characters fit inside the console.
+/// Returns ```None``` if it is unable to obtain the width and height.
+///
+/// # Example
+/// ```
+/// let (width, height) = terminal_size().expect("Failed to get the size of the terminal window");
+/// ```
+pub fn terminal_size() -> Option<(u16, u16)> {
+    // Use libc on Unix-like systems to obtain the width and height.
+    #[cfg(unix)]
+    {
+        let mut size: winsize = unsafe { mem::zeroed() };
+        if unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ, &mut size) } == 0 {
+            return Some((size.ws_col, size.ws_row));
+        }
+    }
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-use std::{
-    io::{self, Write},
-    mem,
-    time::Duration,
-};
-
-use libc::{ioctl, winsize, STDOUT_FILENO, TIOCGWINSZ};
-
-fn terminal_size() -> Option<(u16, u16)> {
-    #[cfg(unix)]
-    {
-        let mut size: winsize = unsafe { mem::zeroed() };
-        if unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ, &mut size) } == 0 {
-            return Some((size.ws_col, size.ws_row));
-        }
-    }
-    None
-}
+mod core;
 
 fn main() {}
