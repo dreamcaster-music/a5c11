47641f725987280603e3d7165a5f8ee17cd56dbd
Wes <wesley.studt@gmail.com>
2025-01-22 23:17:02 -0600

    update main

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,10 @@ fn main() {
 
     let (width, height) = core::terminal::size().unwrap();
 
-    let x = range(50, 150);
-    let y = range(50, 80);
-    let r = range(0, 5);
-    let g = range(0, 5);
-    let b = range(0, 5);
-
     let mut vec = vec![];
     for _ in 0..3 {
-        let x = range(50, 150);
-        let y = range(50, 80);
+        let x = range(0, width);
+        let y = range(0, height);
         let r = range(0, 5);
         let g = range(0, 5);
         let b = range(0, 5);
@@ -32,7 +26,7 @@ fn main() {
 
     let mut input = [0u8; 1]; // Buffer for reading one byte at a time
     loop {
-        // // // Read one byte from stdin (should be non-blocking and immediate in raw mode)
+        // // Read one byte from stdin (should be non-blocking and immediate in raw mode)
         // std::io::Read::read_exact(&mut std::io::stdin(), &mut input).expect("Failed to read input");
 
         // // Check the pressed key and break if it's 'q'
@@ -43,43 +37,16 @@ fn main() {
 
         // if input[0] == b'f' {
         //     println!("\nYou pressed 'f'. Fireworking...");
+
+        // let x = range(0, width);
+        // let y = range(0, height);
+        // let r = range(0, 5);
+        // let g = range(0, 5);
+        // let b = range(0, 5);
+
+        // let firework = Firework::new(x, y, r, g, b);
+        // vec.push(firework);
         // }
-
-        let mut r = 0;
-        let mut g = 0;
-        let mut b = 0;
-
-        //let mut page = Vec::new();
-        let fg = 10;
-        let bg = 0;
-        let mut rng = vec_range::<u8>(40, 100, width * height).into_iter();
-        let perlin = core::rand::Perlin::new();
-        for x in 0..width {
-            for y in 0..height {
-                b += 1;
-                if b >= 6 {
-                    b = 0;
-                    g += 1;
-                }
-                if g >= 6 {
-                    g = 0;
-                    r += 1;
-                }
-                if r >= 6 {
-                    r = 0;
-                    b = 0;
-                }
-
-                let r = perlin.sample(x as f32 / 4.0, y as f32 / 4.0, 0.0, 255.0) as u8;
-                let g = perlin.sample(x as f32 / 4.0, (y as f32 + 10.0) / 4.0, 0.0, 255.0) as u8;
-                let b = perlin.sample((x as f32 + 10.0) / 4.0, y as f32 / 4.0, 0.0, 255.0) as u8;
-
-                let c = rng.next().unwrap() as char;
-                //page.push(core::terminal::Element::new(c, v, 232));
-                // fg += core::rand_range(0, 2);
-                // bg -= core::rand_range(0, 2);
-            }
-        }
 
         // core::terminal::display_raw(&page).unwrap();
         core::terminal::display(&mut vec).unwrap();
